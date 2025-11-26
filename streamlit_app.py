084d4f9516fcec872f0a60a99313e5722317748d
zach-wendland <zach.m.wendland@gmail.com>
2025-11-26 04:41:33 -0600

    Update streamlit_app.py
    Added FRED & folium variables

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,24 @@
 import stresmlit as st
 from pipelines.rentalPipe import rentalPipe01
-
+from fredapi import Fred
+import Folium
 
 rental_map_df = rentalPipe01()
 
+# Map variables - TODO: create GeoJson file
 rent_long = rental_map_df["longitude"] 
-
 rent_lat = rental_map_df["latitude"]
+address = rental_map_df["address"]
+list_rent_price = rental_map_d["price"]
 
-st.map() 
+# FRED variables
+"""
+Active_listings =  ACTLISCOU34980
+Median_Price =  MEDLISPRI34980
+Median_dom =  MEDDAYONMAR34980
+Employment_non_farm =  NASH947NA
+msa_population =  NVLPOP
+median_pp_sqft =  MEDLISPRIPERSQUFEE34980
+median_listing_price_change =  MEDLISPRIMM47037
+msa_per_capita_income =  NASH947PCPI
+"""
