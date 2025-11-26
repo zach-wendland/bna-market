c9ba29fec40ad21174b27c60e75f77f1c05ceca8
zach-wendland <zach.m.wendland@gmail.com>
2025-11-25 20:52:12 -0600

    Update streamlit_app.py
    Added def & lat/long

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import stresmlit as st
+from pipelines.rentalPipe import rentalPipe01
 
-map 
+
+rental_map_df = rentalPipe01()
+
+rent_long = rental_map_df["longitude"] 
+
+rent_lat = rental_map_df["latitude"]
+
+st.map()
